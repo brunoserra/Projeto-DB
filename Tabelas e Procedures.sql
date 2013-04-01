062ff2e48bc08947b1aaa557f7d3de279338b6fa
Bruno Serra <bruno@uptune.com.br>
2013-04-01 14:29:21 -0300

    sfdsdfd

--- Tabelas e Procedures.sql
+++ Tabelas e Procedures.sql
@@ -1,3 +1,15 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4004
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.25)
+# Database: trabalho_final
+# Generation Time: 2013-04-01 17:27:56 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -11,33 +23,68 @@
 # Dump of table Ambiente
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `Ambiente`;
+
 CREATE TABLE `Ambiente` (
   `idAmbiente` int(11) NOT NULL AUTO_INCREMENT,
   `idTipo_Ambiente` int(11) NOT NULL,
   `descricao` varchar(45) DEFAULT NULL,
-  `local` varchar(45) DEFAULT NULL,
+  `nome` varchar(45) DEFAULT NULL,
   `referencia` varchar(45) DEFAULT NULL,
   `capacidade` int(11) DEFAULT NULL,
   PRIMARY KEY (`idAmbiente`),
   KEY `fk_Ambiente_Tipo_Ambiente1` (`idTipo_Ambiente`),
   CONSTRAINT `fk_Ambiente_Tipo_Ambiente1` FOREIGN KEY (`idTipo_Ambiente`) REFERENCES `Tipo_Ambiente` (`idTipo_Ambiente`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `Ambiente` WRITE;
+/*!40000 ALTER TABLE `Ambiente` DISABLE KEYS */;
+
+INSERT INTO `Ambiente` (`idAmbiente`, `idTipo_Ambiente`, `descricao`, `nome`, `referencia`, `capacidade`)
+VALUES
+	(1,1,'lorem ipsum','Jockey sao paulo',NULL,2000);
+
+/*!40000 ALTER TABLE `Ambiente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table Ambiente_has_setor
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Ambiente_has_setor`;
+
+CREATE TABLE `Ambiente_has_setor` (
+  `idAmbiente` int(11) DEFAULT NULL,
+  `idSetor` int(11) DEFAULT NULL,
+  `capacidade` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+LOCK TABLES `Ambiente_has_setor` WRITE;
+/*!40000 ALTER TABLE `Ambiente_has_setor` DISABLE KEYS */;
+
+INSERT INTO `Ambiente_has_setor` (`idAmbiente`, `idSetor`, `capacidade`)
+VALUES
+	(1,1,100);
+
+/*!40000 ALTER TABLE `Ambiente_has_setor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table categoria
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `categoria`;
+
 CREATE TABLE `categoria` (
   `idcategoria` int(11) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(45) DEFAULT NULL,
+  `nome` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idcategoria`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
 
-INSERT INTO `categoria` (`idcategoria`, `descricao`)
+INSERT INTO `categoria` (`idcategoria`, `nome`)
 VALUES
 	(1,'festival');
 
@@ -48,24 +95,30 @@ UNLOCK TABLES;
 # Dump of table Compra
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `Compra`;
+
 CREATE TABLE `Compra` (
   `idCompra` int(11) NOT NULL AUTO_INCREMENT,
   `idUsuario` int(11) NOT NULL,
   `data` date DEFAULT NULL,
   `total` float DEFAULT NULL,
   `qtd` int(11) DEFAULT NULL,
+  `tipo` int(1) DEFAULT '0' COMMENT '0=compra 1= reserva',
   PRIMARY KEY (`idCompra`),
   KEY `fk_Compra_Usuario` (`idUsuario`),
   CONSTRAINT `fk_Compra_Usuario` FOREIGN KEY (`idUsuario`) REFERENCES `Usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `Compra` WRITE;
 /*!40000 ALTER TABLE `Compra` DISABLE KEYS */;
 
-INSERT INTO `Compra` (`idCompra`, `idUsuario`, `data`, `total`, `qtd`)
+INSERT INTO `Compra` (`idCompra`, `idUsuario`, `data`, `total`, `qtd`, `tipo`)
 VALUES
-	(1,1,'2013-03-25',400,1),
-	(2,1,'2013-03-25',400,1);
+	(12,1,'2013-03-26',800,1,0),
+	(13,1,'2013-03-26',800,1,0),
+	(14,2,'2013-03-26',400,1,0),
+	(15,2,'2013-03-26',400,1,0),
+	(16,2,'2013-03-26',400,1,0);
 
 /*!40000 ALTER TABLE `Compra` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -74,9 +127,15 @@ UNLOCK TABLES;
 # Dump of table Compra_has_ingresso
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `Compra_has_ingresso`;
+
 CREATE TABLE `Compra_has_ingresso` (
   `idCompra` int(11) DEFAULT NULL,
-  `idIngresso` int(11) DEFAULT NULL
+  `idIngresso` int(11) DEFAULT NULL,
+  KEY `idCompra` (`idCompra`),
+  KEY `idIngresso` (`idIngresso`),
+  CONSTRAINT `Compra_has_ingresso_ibfk_1` FOREIGN KEY (`idCompra`) REFERENCES `Compra` (`idCompra`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Compra_has_ingresso_ibfk_2` FOREIGN KEY (`idIngresso`) REFERENCES `Ingresso` (`idIngresso`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `Compra_has_ingresso` WRITE;
@@ -84,8 +143,11 @@ LOCK TABLES `Compra_has_ingresso` WRITE;
 
 INSERT INTO `Compra_has_ingresso` (`idCompra`, `idIngresso`)
 VALUES
-	(1,1),
-	(2,2);
+	(12,6),
+	(13,7),
+	(14,4),
+	(15,5),
+	(16,8);
 
 /*!40000 ALTER TABLE `Compra_has_ingresso` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -94,25 +156,29 @@ UNLOCK TABLES;
 # Dump of table Evento
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `Evento`;
+
 CREATE TABLE `Evento` (
   `idEvento` int(11) NOT NULL AUTO_INCREMENT,
   `idcategoria` int(11) NOT NULL,
+  `idAmbiente` int(11) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
   `descricao` varchar(45) DEFAULT NULL,
   `data` date DEFAULT NULL,
-  `resumo` varchar(45) DEFAULT NULL,
-  `imagem` varchar(45) DEFAULT NULL,
-  `nome` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idEvento`),
   KEY `fk_Evento_categoria1` (`idcategoria`),
   CONSTRAINT `fk_Evento_categoria1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `Evento` WRITE;
 /*!40000 ALTER TABLE `Evento` DISABLE KEYS */;
 
-INSERT INTO `Evento` (`idEvento`, `idcategoria`, `descricao`, `data`, `resumo`, `imagem`, `nome`)
+INSERT INTO `Evento` (`idEvento`, `idcategoria`, `idAmbiente`, `nome`, `descricao`, `data`)
 VALUES
-	(1,1,'aiuedhiauehdiaudh','2012-12-12','aduhaisuhd','audha','Lollapalooza');
+	(1,1,1,'Lollapalooza','aiuedhiauehdiaudh','2013-12-25'),
+	(7,1,1,'planeta terra 2013','O maior evento hipster do brasil','2013-05-25'),
+	(17,1,1,'Planeta Terra 2014','O maior evento hipster do brasil','2013-05-25'),
+	(18,1,1,'Planeta Terra 2015','O maior evento hipster do brasil','2013-03-27');
 
 /*!40000 ALTER TABLE `Evento` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -120,6 +186,8 @@ UNLOCK TABLES;
 
 # Dump of table Forma_Pagamento
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Forma_Pagamento`;
 
 CREATE TABLE `Forma_Pagamento` (
   `idForma_Pagamento` int(11) NOT NULL AUTO_INCREMENT,
@@ -143,26 +211,36 @@ UNLOCK TABLES;
 # Dump of table Ingresso
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `Ingresso`;
+
 CREATE TABLE `Ingresso` (
   `idIngresso` int(11) NOT NULL AUTO_INCREMENT,
   `idEvento` int(11) NOT NULL,
   `idSetor` int(11) DEFAULT NULL,
   `valor` float DEFAULT NULL,
-  `tipo` varchar(1) DEFAULT NULL,
   `status` int(1) DEFAULT NULL,
   PRIMARY KEY (`idIngresso`,`idEvento`),
   KEY `fk_Ingresso_Evento1` (`idEvento`),
   CONSTRAINT `fk_Ingresso_Evento1` FOREIGN KEY (`idEvento`) REFERENCES `Evento` (`idEvento`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `Ingresso` WRITE;
 /*!40000 ALTER TABLE `Ingresso` DISABLE KEYS */;
 
-INSERT INTO `Ingresso` (`idIngresso`, `idEvento`, `idSetor`, `valor`, `tipo`, `status`)
+INSERT INTO `Ingresso` (`idIngresso`, `idEvento`, `idSetor`, `valor`, `status`)
 VALUES
-	(1,1,1,400,'1',0),
-	(2,1,1,400,'1',0),
-	(3,1,1,400,'1',1);
+	(1,1,1,400,0),
+	(2,1,1,400,0),
+	(3,1,1,400,0),
+	(4,1,1,400,0),
+	(5,1,1,400,0),
+	(6,17,1,800,1),
+	(7,17,1,800,0),
+	(8,1,1,400,0),
+	(9,1,1,300,NULL),
+	(10,1,1,300,NULL),
+	(11,1,1,300,NULL),
+	(12,1,1,300,NULL);
 
 /*!40000 ALTER TABLE `Ingresso` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -171,17 +249,34 @@ UNLOCK TABLES;
 # Dump of table Integrantes
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `Integrantes`;
+
 CREATE TABLE `Integrantes` (
   `idIntegrantes` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   `funcao` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idIntegrantes`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
+LOCK TABLES `Integrantes` WRITE;
+/*!40000 ALTER TABLE `Integrantes` DISABLE KEYS */;
+
+INSERT INTO `Integrantes` (`idIntegrantes`, `nome`, `funcao`)
+VALUES
+	(1,'Maria',NULL),
+	(2,'Joana',NULL),
+	(3,'Paula',NULL),
+	(4,'Bruno Serra',NULL),
+	(5,'Marcos',NULL);
+
+/*!40000 ALTER TABLE `Integrantes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table Integrantes_has_Evento
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Integrantes_has_Evento`;
 
 CREATE TABLE `Integrantes_has_Evento` (
   `Integrantes_idIntegrantes` int(11) NOT NULL,
@@ -193,10 +288,28 @@ CREATE TABLE `Integrantes_has_Evento` (
   CONSTRAINT `fk_Integrantes_has_Evento_Integrantes1` FOREIGN KEY (`Integrantes_idIntegrantes`) REFERENCES `Integrantes` (`idIntegrantes`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+LOCK TABLES `Integrantes_has_Evento` WRITE;
+/*!40000 ALTER TABLE `Integrantes_has_Evento` DISABLE KEYS */;
+
+INSERT INTO `Integrantes_has_Evento` (`Integrantes_idIntegrantes`, `Evento_idEvento`)
+VALUES
+	(1,17),
+	(3,17),
+	(4,17),
+	(5,17),
+	(1,18),
+	(3,18),
+	(4,18),
+	(5,18);
+
+/*!40000 ALTER TABLE `Integrantes_has_Evento` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table Pagamento
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Pagamento`;
 
 CREATE TABLE `Pagamento` (
   `idPagamento` int(11) NOT NULL AUTO_INCREMENT,
@@ -204,40 +317,45 @@ CREATE TABLE `Pagamento` (
   `idCompra` int(11) DEFAULT NULL,
   `valor` float DEFAULT NULL,
   `vencimento` date DEFAULT NULL,
+  `status` int(1) DEFAULT '0' COMMENT '0= em aberto 1 = pago',
   PRIMARY KEY (`idPagamento`,`idForma_Pagamento`),
   KEY `fk_Pagamento_Forma_Pagamento1` (`idForma_Pagamento`),
   CONSTRAINT `fk_Pagamento_Forma_Pagamento1` FOREIGN KEY (`idForma_Pagamento`) REFERENCES `Forma_Pagamento` (`idForma_Pagamento`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `Pagamento` WRITE;
 /*!40000 ALTER TABLE `Pagamento` DISABLE KEYS */;
 
-INSERT INTO `Pagamento` (`idPagamento`, `idForma_Pagamento`, `idCompra`, `valor`, `vencimento`)
+INSERT INTO `Pagamento` (`idPagamento`, `idForma_Pagamento`, `idCompra`, `valor`, `vencimento`, `status`)
 VALUES
-	(4,2,1,133.333,'2013-04-25'),
-	(5,2,1,133.333,'2013-05-25'),
-	(6,2,1,133.333,'2013-06-25'),
-	(7,1,2,400,'2013-03-25');
+	(4,2,1,133.333,'2013-04-25',0),
+	(5,2,1,133.333,'2013-05-25',0),
+	(6,2,1,133.333,'2013-06-25',0),
+	(7,1,1,400,'2013-03-25',0),
+	(8,1,1,400,'2013-03-25',0),
+	(9,1,4,400,'2013-03-25',0),
+	(10,1,5,0,'2013-03-25',0),
+	(11,1,6,0,'2013-03-25',0),
+	(12,1,7,0,'2013-03-25',1),
+	(13,1,8,0,'2013-03-25',1),
+	(14,1,9,400,'2013-03-25',0),
+	(15,1,13,800,'2013-01-26',0),
+	(16,1,13,800,'2013-09-26',1),
+	(17,1,13,400,'2013-03-26',1),
+	(18,1,13,400,'2013-03-26',1),
+	(19,1,13,400,'2013-03-26',1),
+	(23,2,16,133,'2013-03-27',0),
+	(24,2,16,133,'2013-04-27',0),
+	(25,2,16,133,'2013-05-27',0);
 
 /*!40000 ALTER TABLE `Pagamento` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table Reserva
-# ------------------------------------------------------------
-
-CREATE TABLE `Reserva` (
-  `idReserva` int(11) NOT NULL AUTO_INCREMENT,
-  `idUsuario` int(11) NOT NULL,
-  PRIMARY KEY (`idReserva`,`idUsuario`),
-  KEY `fk_Reserva_Usuario1` (`idUsuario`),
-  CONSTRAINT `fk_Reserva_Usuario1` FOREIGN KEY (`idUsuario`) REFERENCES `Usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
 # Dump of table Setor
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Setor`;
 
 CREATE TABLE `Setor` (
   `idSetor` int(11) NOT NULL AUTO_INCREMENT,
@@ -260,16 +378,29 @@ UNLOCK TABLES;
 # Dump of table Tipo_Ambiente
 # ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `Tipo_Ambiente`;
+
 CREATE TABLE `Tipo_Ambiente` (
   `idTipo_Ambiente` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idTipo_Ambiente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
+LOCK TABLES `Tipo_Ambiente` WRITE;
+/*!40000 ALTER TABLE `Tipo_Ambiente` DISABLE KEYS */;
+
+INSERT INTO `Tipo_Ambiente` (`idTipo_Ambiente`, `descricao`)
+VALUES
+	(1,'Jockey');
+
+/*!40000 ALTER TABLE `Tipo_Ambiente` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table Usuario
 # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Usuario`;
 
 CREATE TABLE `Usuario` (
   `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
